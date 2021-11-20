539df78ac6bb605e37aeb2f3d2637e270872dad5
Greg Heiman <gregheiman02@gmail.com>
2021-11-19 18:16:23 -0600

    squash! Added makefile to automate builds.

---
+++ src/include/ray.h
@@ -0,0 +1,23 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray
+{
+ public:
+    ray() {}
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+    point3 origin() const;
+
+    vec3 direction() const;
+
+    point3 at(double_t t) const;
+
+ private:
+    point3 orig;
+    vec3 dir;
+};
+
+#endif
---
+++ src/ray.cpp
@@ -0,0 +1,23 @@
+#include "include/ray.h"
+
+point3 ray::origin() const
+{
+    return orig;
+}
+
+point3 ray::direction() const
+{
+    return dir;
+}
+
+/*
+ * Ray can be thought of as function P(t) = A + tb
+ * P is the 3D position along a line in 3D
+ * A is the ray origin
+ * b is the ray direction
+ * t moves the point along the ray
+ */
+point3 ray::at(double_t t) const
+{
+    return orig + t*dir;
+}
